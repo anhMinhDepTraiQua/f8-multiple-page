4edcf1fcedd30d38e600d5393fb45c34d3a73225
NgoMinh <ducanhbanche@gmail.com>
2025-06-30 22:35:04 +0700

    acc

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { ViteEjsPlugin } from "vite-plugin-ejs";
 import path from "path";
 
 export default defineConfig({
+    base: "/f8-multiple-page/",
     root: "src",
     publicDir: "../public",
     build: {
